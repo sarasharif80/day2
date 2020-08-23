970d5519ee976079abceee4a3552b3c757e78329
saras <ag@gmail.com>
2020-08-22 23:25:33 -0400

    Day 2 Package practice

---
+++ src/comments/CommentsInJava.java
@@ -0,0 +1,39 @@
+package comments;
+/**
+ * 
+ * @author saras
+ *
+ */
+public class CommentsInJava {
+
+	public static void main (String[]arg) {
+	
+	
+	//System.out.println("Java Day 2 Class");
+		//----------- Multi line comment ------------
+		/* system.out.println("salam");
+		 
+		 */
+	
+	
+	// this is single line comment in Java
+	System.out.println("After comment");
+	
+	System.out.println("Multi line comments in Java");
+	
+	/*
+	 * put any comments here
+	 *and here 
+	 *and here
+	 */
+	/**
+	 * salam khareh
+	 * java sakhteh
+	 * sakhteh sakhteh
+	 * this is ducumantion comments
+	 * 
+	 */
+	}
+	
+	
+}
---
+++ src/variables/Variablesinjava.java
@@ -0,0 +1,88 @@
+package variables;
+
+public class Variablesinjava {
+
+	public static void main (String []arg) {
+
+		//Declaring a variable
+		/*
+		 * syntax for --> Declaring a variable
+		 *     DaytaType VariableName;
+		 * 
+	*/
+	//DaytaType VariableName;	
+		//int age;
+		// int faivoritenumber;
+		/**
+		 * syntax for Declaring a variable and assigning a value
+		 * 
+		 *      DaytaType variableName=variableValue;
+		 */
+		//DaytaType variableName=variableValue;
+		
+		int luckyNumber=7;
+		int oddNumber=11;
+		System.out.println(luckyNumber);
+		System.out.println(oddNumber);
+		
+		//Byte example
+		byte count=44;
+		System.out.println(count);
+		
+		//short Example
+		short zipcode=1234;
+		System.out.println(zipcode);
+		
+		/*int Example*/
+		int size=444;
+		System.out.println(size);
+		
+		/** long example*/
+		long pupulation=123456789L;
+		System.out.println(pupulation);
+		
+		//float Example
+		float inches=64.345f;
+		System.out.println(inches);
+		
+		//double example
+		double price= 5.99;
+		System.out.println(price);
+		
+		/**boolean example*/
+
+		boolean isJavaFun=true;
+		boolean amITired=false;
+		System.out.println(isJavaFun);
+		System.out.println(amITired);
+		
+		// character -->char example
+		char mygrade= 'A';
+		char gender= 'F';
+		System.out.println(mygrade);
+		System.out.println(gender);
+		
+		/** string example*/
+		String myName = "my name goes here";
+		System.out.println(myName);
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+			
+		
+		
+		
+		
+		
+		
+}
+	
+}
